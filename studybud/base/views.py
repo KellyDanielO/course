c32b3011fea8ff194228ad94742140d725909f55
KellyDanielO <kizdver@gmail.com>
2023-08-23 15:34:25 +0100

    .

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     {'id': 1, 'name': 'Let\'s learn python!'},
@@ -19,3 +20,23 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
