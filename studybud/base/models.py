0b392aaf51bd6aa9eb8eb3e5c03d1a2d8fa7932b
KellyDanielO <kizdver@gmail.com>
2023-08-24 13:40:16 +0100

    .

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -33,5 +33,9 @@ class Message(models.Model):
     updated = models.DateField(auto_now=True)
     created = models.DateField(auto_now_add=True)
 
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.body[:50]
