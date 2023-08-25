00217fb4be23716f1124e2245ee7d5ec6b1ad37e
KellyDanielO <kizdver@gmail.com>
2023-08-25 19:00:36 +0100

    .

--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,9 +1,26 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    room = Room.objects.all()
+    serializer = RoomSerializer(room, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room)
+    return Response(serializer.data)
